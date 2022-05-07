7ad356f6eb483e877382e2c9117bf990190f71f1
Lightwand <karlavohra@yahoo.com>
2022-05-07 11:37:48 -0700

    Initial Commit

---
+++ M3_L2_GitHub_and_Xcode_Practice_v01/M3_L2_GitHub_and_Xcode_Practice_v01App.swift
@@ -0,0 +1,17 @@
+//
+//  M3_L2_GitHub_and_Xcode_Practice_v01App.swift
+//  M3_L2_GitHub_and_Xcode_Practice_v01
+//
+//  Created by Karla Vohra on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct M3_L2_GitHub_and_Xcode_Practice_v01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
